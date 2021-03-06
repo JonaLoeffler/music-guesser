088f0b4e0e03562e6dd70532f40147458b67b695
Jona Löffler <jona.loeffler@online.de>
2021-03-06 21:12:27 +0100

    Add scaffolding for rooms

---
+++ app/Http/Controllers/RoomController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Room;
+use Illuminate\Http\Request;
+
+class RoomController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return response()->make('todo');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Room  $room
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Room $room)
+    {
+        return response()->make('todo');
+    }
+}
