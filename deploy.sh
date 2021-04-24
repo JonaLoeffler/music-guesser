201b830d06a019fb3542ae3c3c5b889b612c8e7a
Jona Löffler <jona.loeffler@online.de>
2021-04-24 18:38:19 +0200

    Add deployment script

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -e
+
+echo "Deploying application ..."
+
+# Enter maintenance mode
+(php artisan down) || true
+    # Update codebase
+    git fetch origin deploy
+    git reset --hard origin/deploy
+
+    # Install dependencies based on lock file
+    docker exec -it which-track-app composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
+
+    # Migrate database
+    docker exec -it which-track-app php artisan migrate --force
+
+    # Note: If you're using queue workers, this is the place to restart them.
+    # ...
+
+    # Clear cache
+    docker exec -it which-track-app php artisan optimize
+# Exit maintenance mode
+docker exec -it which-track-app php artisan up
+
+echo "Application deployed!"
+
